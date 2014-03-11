72f7c2c6b8525023228f05f55ef266e01f0b0985
Scott Jehl <scottjehl@gmail.com>
2014-03-11 16:11:57 -0500

    add cvv stuff

--- src/patterns/credit/validator.credit.js
+++ src/patterns/credit/validator.credit.js
@@ -25,6 +25,34 @@
 		return $( this.element ).closest( "form" ).find( "[data-validate=credit]" );
 	};
 
+	Validator.prototype._findCvvField = function() {
+		return $( this.element ).closest( "form" ).find( "[data-validate=cvv]" );
+	};
+	
+	Validator.prototype.validatecvv = function( number ){
+		if( isNaN( parseInt( number, 10 ) ) || parseInt( number, 10 ) < 0 ){
+			return;
+		}
+
+		var cc = this._findCreditField(),
+			result = false,
+			card = this._getCreditType( cc[ 0 ].value );
+
+		if( card && card.id ){
+			result = number.toString().length === parseInt( card.cvvlength, 10 );
+		}
+
+		return result;
+	};
+
+	Validator.prototype.messagecvv = function( number ){
+		var copy = this.data.copy.validator,
+			cc = this._findCreditField(),
+			card = this._getCreditType( cc[ 0 ].value );
+
+		return card && copy.cvv[ card.id ].message || copy.cvv.message;
+	};
+
 	Validator.prototype.validatecredit = function( value ){
 		var number = value.replace( /\s/g , '').replace( /-/g, ''),
 			result = false,
